2c78d6f937cb9ec597dd13f09c9fea65a746502c
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-15 16:50:24 +0000

    added code

---
+++ lab6.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+from shifter import Shifter
+import time
+from led8x8 import LED8x8
+
+data, latch, clock = 23, 24, 25
+myLED = LED8x8(data, latch, clock)
+while True:
+  myLED.display()

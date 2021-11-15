1479c4263ff0ea3a238afc050042a5d07f17b2a5
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-15 17:01:40 +0000

    a

--- led8x8.py
+++ led8x8.py
@@ -15,5 +15,5 @@ class LED8x8():
     for i in range(8):
       self.shifter.shiftByte(pattern[i])
       self.shifter.shiftByte(row[i])
-    self.shifter.latch(self.shifter.latchPin)
+    self.shifter.latch(self.latch)
     time.sleep(0.001)

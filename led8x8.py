03d6c86505aa34c72bef93c62ef7f36f82d9b92a
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-15 16:56:29 +0000

    row[i]

--- led8x8.py
+++ led8x8.py
@@ -6,6 +6,7 @@ global pattern
 pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 
 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
+row = [0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000]
 class LED8x8():
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)   
@@ -13,7 +14,6 @@ class LED8x8():
   def display(self):
     for i in range(8):
       self.shifter.shiftByte(pattern[i])
-      self.shifter.shiftByte(1 << i)
-      print(pattern[i])
+      self.shifter.shiftByte(row[i])
     self.shifter.ping(self.shifter.latchPin)
     time.sleep(0.001)

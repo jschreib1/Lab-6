5e8a66b2a74a555bdcc311135b63ea20be47ed15
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-11-17 00:41:32 +0000

    new approach

--- lab6.py
+++ lab6.py
@@ -9,24 +9,24 @@ data, latch, clock = 23, 24, 25
 myLED = LED8x8(data, latch, clock)
 myShifter = Shifter(data, latch, clock)
 random.seed()
-
-def move(space):
-  space += random.randint(-1,1)
+'''
+def move(row, column):
+  move += random.randint(-1,1)
   while space < 1 or space > 8:
     space += random.randint(-1,1)
-
+'''
 global row, column
-row = random.randint(0,7) #initial row for random walk
-myLED.smiley[0] = myLED.pattern[row]
-print(row)
-column = random.randint(0,7) #initial column for random walk
-myLED.smiley[1] = myLED.pattern[column]
-print(column)
+#row = random.randint(0,7) #initial row for random walk
+#myLED.smiley[0] = myLED.pattern[row]
+#column = random.randint(0,7) #initial column for random walk
+#myLED.smiley[1] = myLED.pattern[column]
 
 try:
   while True:
-    #myLED.smiley[0] = move(row)
-    #myLED.smiley[1] = move(column)
+    myLED.smiley[0] = random.randint(1,8)
+    myLED.smiley[1] = random.randint(1,8)
+    myLED.shifter.shiftByte((1 << (myLED.smiley[0]-1)))
+    myLED.shifter.shiftByte((1 << (myLED.smiley[1]-1)))
     #myLED.display() commented out for the random walk
     #time.sleep(0.001)
     time.sleep(0.1)
